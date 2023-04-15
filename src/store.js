604e8fab597fd011fc49078a1d447f00e285dfda
diter14 <diter.terrones14@gmail.com>
2023-04-14 22:34:49 -0500

    inicializa archivo store para manejar el estado global

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, createLogger } from "vuex";
+
+const debug = process.env.NODE_ENV !== "production";
+
+export default createStore({
+  state() {
+    return {};
+  },
+  actions: {},
+  mutations: {},
+  plugins: debug ? [createLogger()] : [],
+});
