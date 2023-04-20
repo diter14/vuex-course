cf6369965dd6464ceac41329157e91dd4a999e84
diter14 <diter.terrones14@gmail.com>
2023-04-19 21:41:17 -0500

    crea env.local y registra constantes utilitarias para acceder a las variables de entorno

---
+++ src/services/firebase.js
@@ -0,0 +1,22 @@
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTHDOMAIN,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECTID,
+  FIREBASE_STORAGEBUCKET,
+} from "@/utils/constants";
+import { initializeApp } from "firebase";
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTHDOMAIN,
+  projectId: FIREBASE_PROJECTID,
+  storageBucket: FIREBASE_STORAGEBUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const FIREBASE_API_KEY = process.env.VUE_APP_APIKEY || "";
+export const FIREBASE_AUTHDOMAIN = process.env.VUE_APP_AUTHDOMAIN || "";
+export const FIREBASE_PROJECTID = process.env.VUE_APP_PROJECTID || "";
+export const FIREBASE_STORAGEBUCKET = process.env.VUE_APP_STORAGEBUCKET || "";
+export const FIREBASE_MESSAGING_SENDER_ID =
+  process.env.VUE_APP_MESSAGING_SENDER_ID || "";
+export const FIREBASE_APP_ID = process.env.VUE_APP_FIREBASE_APP_ID || "";
