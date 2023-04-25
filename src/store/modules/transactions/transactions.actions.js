5e8033d1e672542f0acdce171f7d44cacb6ea158
diter14 <diter.terrones14@gmail.com>
2023-04-24 21:39:00 -0500

    registra actions para módulo transactions

---
+++ src/store/modules/transactions/transactions.actions.js
@@ -0,0 +1,42 @@
+import TransactionService from "@/services/TransactionService";
+import {
+  CREATE_TRANSACTION_ERROR,
+  CREATE_TRANSACTION_REQUEST,
+  CREATE_TRANSACTION_SUCCESS,
+  GET_TRANSACTIONS_ERROR,
+  GET_TRANSACTIONS_REQUEST,
+  GET_TRANSACTIONS_SUCCESS,
+} from "./transactions.types";
+
+const createTransaction = async (
+  { commit },
+  { description, amount, type, category, date }
+) => {
+  try {
+    let transactionCreatedId = await TransactionService.createTransaction({
+      description,
+      amount,
+      type,
+      category,
+      date,
+    });
+    commit(CREATE_TRANSACTION_REQUEST, transactionCreatedId);
+    commit(CREATE_TRANSACTION_SUCCESS);
+  } catch (error) {
+    commit(CREATE_TRANSACTION_ERROR, error);
+  }
+};
+
+const fetchTransactions = async ({ commit }) => {
+  try {
+    commit(GET_TRANSACTIONS_REQUEST);
+    let transactions = TransactionService.getTransactions();
+    commit(GET_TRANSACTIONS_SUCCESS, transactions);
+  } catch (error) {
+    commit(GET_TRANSACTIONS_ERROR);
+  }
+};
+export default {
+  createTransaction,
+  fetchTransactions,
+};
