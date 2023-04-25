d3044e828f7411db22e35427d2f94c1fd7193b3c
diter14 <diter.terrones14@gmail.com>
2023-04-24 21:40:10 -0500

    agrega módulo transactions al store global

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { createStore, createLogger } from "vuex";
 import categories from "./modules/categories";
+import transactions from "./modules/transactions";
 
 const debug = process.env.NODE_ENV !== "production";
 
 export default createStore({
   modules: {
     categories,
+    transactions,
   },
   plugins: debug ? [createLogger()] : [],
 });
