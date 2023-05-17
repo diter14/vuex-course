b5bf1f04b3ffb448ae787e7b823478bed6f130de
diter14 <diter.terrones14@gmail.com>
2023-05-16 22:27:11 -0500

    fix: agrega hack de configuración para ocultar alertas de vue en consola

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import store from "./store/store";
 import "bulma/css/bulma.min.css";
 
 const app = createApp(App);
-
+// Hack para ocultar las alertas
+app.config.warnHandler = () => null;
+//
 app.use(router);
 app.use(store);
 app.mount("#app");
